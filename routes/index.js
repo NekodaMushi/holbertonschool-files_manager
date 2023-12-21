26f1add4ffe5cd21e52759f70fc4e96440b52296
NekodaMushi <study.fabien@gmail.com>
2023-12-21 13:35:07 +0100

    API

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const AppController = require("../controllers/AppController");
+const UsersController = require("../controllers/UsersController");
+const AuthController = require("../controllers/AuthController");
+const FilesController = require("../controllers/FilesController");
+
+const router = express.Router();
+
+router.get("/status", AppController.getStatus);
+router.get("/stats", AppController.getStats);
+
+export default router;
